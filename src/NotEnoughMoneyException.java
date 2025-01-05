4f7e6652b3eeb8aaf3f9686e799a2e02757f8364
Maciej Janowski <maciekjanowski3@gmail.com>
2025-01-05 23:48:12 +0100

    NotEnoughMoneyException.java

--- src/NotEnoughMoneyException.java
+++ src/NotEnoughMoneyException.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
 
-public class NotEnoughMoneyException {
+public class NotEnoughMoneyException extends Exception {
+    public NotEnoughMoneyException (String message) { super(message);}
+}
+
+class Account {
+    private String owner;
+    private int balance;
+    private String accountNumber;
+
+    public Account(String owner, int balance, String accountNumber) {
+        this.owner = owner;
+        this.balance = balance;
+        this.accountNumber = accountNumber;
+    }
+
+    public int getBalance() { return balance; }
+
+    public void withdrawal(int amount) throws NotEnoughMoneyException {
+        if (balance >= amount) {
+            balance = balance - amount;
+        } else {
+            throw new NotEnoughMoneyException("Not enough money/funds in the account.");
+        }
+    }
 }
